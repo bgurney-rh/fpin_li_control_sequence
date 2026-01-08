808d1f8f65c10ddc7783336c4ef20229284f647e
Bryan Gurney <bgurney@redhat.com>
2026-01-08 13:44:08 -0500

    Remove control_host_port functions and driver parameter
    Now that the reset_marginal_rport function can reset the "Marginal" link
    status via sysfs, the driver-specific functions for controlling the host
    port state can be removed, which simplifies the test script.
    
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- fpin_li_control_sequence_script.sh
+++ fpin_li_control_sequence_script.sh
@@ -20,15 +20,12 @@ set -e
 #
 # fpin_li_control_sequence_script.sh parameters:
 # 1: Device node for test namespace.
-# 2: Driver of host bus adapter to control
+# 2: SCSI FC host ID to control
 # 3: Port ID on fibre channel switch to send FPIN event
 # 4: non-marginal optimized interface on storage array to control
 #
 # Example:
 # $ bash fpin_li_control_sequence_script.sh /dev/nvme4n1 qla2xxx 021700 fc1_01_5d
-
-# TODO: hardcoded items
-# - qla2xxx: bus address of HBA to unbind and bind
 #
 # Interesting items to find:
 # - primary array controller (ANA state "optimized")
@@ -40,17 +37,16 @@ POST_CHECK_DELAY=2
 
 if [ ! "$1" ] || [ ! "$2" ] || [ ! "$3" ]
 then
-	echo "Usage: $0 <device> <driver> <scsi_host_id> <port_id> <non_marginal_opt_path>"
+	echo "Usage: $0 <device> <scsi_host_id> <port_id> <non_marginal_opt_path>"
 	echo "WARNING: Be sure to use the correct port ID for this host."
 	echo "(Run 'grep . /sys/class/fc_host/host*/port_* to display')"
 	exit 2
 fi
 
 DEVPATH=$1
-DRIVER=$2
-SCSIHOSTID=$3
-PORTID=$4
-NON_MARGINAL_OPTIMIZED=$5
+SCSIHOSTID=$2
+PORTID=$3
+NON_MARGINAL_OPTIMIZED=$4
 
 if [ ! "$FCSWITCH" ] || [ ! "$STORARRAY" ]
 then
@@ -101,48 +97,12 @@ control_target_path() {
 	date; sshpass -e ssh "$STORARRAY" "net int modify -vserver fcqe1 -lif $NON_MARGINAL_OPTIMIZED -status-admin $PATH_STATE"
 }
 
-control_host_port_down() {
-	case $DRIVER in
-	"qla2xxx")
-		date; echo "0000:b4:00.0" | sudo tee /sys/bus/pci/drivers/qla2xxx/unbind
-	;;
-	"lpfc")
-		date; echo "down" | sudo tee /sys/class/scsi_host/"$SCSIHOSTID"/link_state
-	;;
-	esac
-}
-
-control_host_port_up() {
-	case $DRIVER in
-	"qla2xxx")
-		date; echo "0000:b4:00.0" | sudo tee /sys/bus/pci/drivers/qla2xxx/bind
-	;;
-	"lpfc")
-		date; echo "up" | sudo tee /sys/class/scsi_host/"$SCSIHOSTID"/link_state
-	;;
-	esac
-}
-
 reset_marginal_rport() {
 	echo "Online" | sudo tee /sys/class/fc_host/"$SCSIHOSTID"/device/rport*/fc_remote_ports/*/port_state
 }
 
 
 date
-echo "Driver: $DRIVER"
-
-case $DRIVER in
-"qla2xxx")
-	echo "Using qla2xxx; requires unbind/bind for port control"
-	;;
-"lpfc")
-	echo "Using lpfc; can use scsi_host link_state sysfs file for control"
-	;;
-*)
-	echo "Unknown driver; use 'qla2xxx' or 'lpfc'"
-	exit 4
-	;;
-esac
 
 echo "Sending FPIN Link Integrity event..."
 send_fpin_link_integrity_event
